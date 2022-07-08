c676688b819689f77505f3ad15376f70ed811fed
bzceval <busraaceval@gmail.com>
2022-07-09 01:46:17 +0300

    add header skeloton

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 document.getElementById("openNav").addEventListener("mouseover", ()=> {
     document.getElementById("fullScreenMenu").classList.add("h-100")
 })
-document.querySelector(".close-btn").addEventListener("mouseover", ()=> {
+document.querySelector(".close-home").addEventListener("mouseover", ()=> {
+    document.getElementById("fullScreenMenu").classList.remove("h-100")
+})
+document.querySelector(".close-adjective").addEventListener("mouseover", ()=> {
+    document.getElementById("fullScreenMenu").classList.remove("h-100")
+})
+document.querySelector(".close-blog").addEventListener("mouseover", ()=> {
+    document.getElementById("fullScreenMenu").classList.remove("h-100")
+})
+document.querySelector(".close-contact").addEventListener("mouseover", ()=> {
     document.getElementById("fullScreenMenu").classList.remove("h-100")
 })
