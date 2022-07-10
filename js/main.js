fbfa25ea0db6df58b90d232d37facd92646c57e3
bzceval <busraaceval@gmail.com>
2022-07-10 17:08:52 +0300

    create web dropdown

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 document.getElementById("openNav").addEventListener("mouseover", ()=> {
     document.getElementById("fullScreenMenu").classList.add("h-100")
 })
-document.querySelector(".close-home").addEventListener("mouseover", ()=> {
-    document.getElementById("fullScreenMenu").classList.remove("h-100")
-})
-document.querySelector(".close-adjective").addEventListener("mouseover", ()=> {
-    document.getElementById("fullScreenMenu").classList.remove("h-100")
-})
-document.querySelector(".close-blog").addEventListener("mouseover", ()=> {
-    document.getElementById("fullScreenMenu").classList.remove("h-100")
-})
-document.querySelector(".close-contact").addEventListener("mouseover", ()=> {
+document.querySelector(".close-btn").addEventListener("click", ()=> {
     document.getElementById("fullScreenMenu").classList.remove("h-100")
 })
