934f3e23079464faf3934e578a8198cc3322115f
bzceval <busraaceval@gmail.com>
2022-07-22 05:43:21 +0300

    change landing page width

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("openNav").addEventListener("mouseover", ()=> {
-//     document.getElementById("fullScreenMenu").classList.add("h-100")
-// })
-// document.querySelector(".close-btn").addEventListener("click", ()=> {
-//     document.getElementById("fullScreenMenu").classList.remove("h-100")
-// })
