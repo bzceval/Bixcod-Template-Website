ce0606d21e9797598dbe353ff22a5ce995c08691
bzceval <busraaceval@gmail.com>
2022-07-22 05:02:10 +0300

    add mouse over effect with bootstrap

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-document.getElementById("openNav").addEventListener("mouseover", ()=> {
-    document.getElementById("fullScreenMenu").classList.add("h-100")
-})
-document.querySelector(".close-btn").addEventListener("click", ()=> {
-    document.getElementById("fullScreenMenu").classList.remove("h-100")
-})
+// document.getElementById("openNav").addEventListener("mouseover", ()=> {
+//     document.getElementById("fullScreenMenu").classList.add("h-100")
+// })
+// document.querySelector(".close-btn").addEventListener("click", ()=> {
+//     document.getElementById("fullScreenMenu").classList.remove("h-100")
+// })
